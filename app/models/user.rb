5cba7527681a9598e6f04068ed1ca7f9975caea7
Juanjo Bazán <jjbazan@gmail.com>
2017-06-16 17:05:37 +0200

    fixes user locale validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,7 @@ class User < ActiveRecord::Base
   validates :official_level, inclusion: {in: 0..5}
   validates :terms_of_service, acceptance: { allow_nil: false }, on: :create
 
-  validates :locale, inclusion: {in: I18n.available_locales.map(&:to_s),
-                                 allow_nil: true}
+  validates :locale, inclusion: {in: I18n.available_locales.map(&:to_s}, allow_nil: true
 
   validates_associated :organization, message: false
 
