662d07852f9d6d07ead536f5be638f90fadc89e9
Ignacio Aliende García <ialiendeg@gmail.com>
2017-11-10 09:31:46 +0000

    Removes tls from email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     user_name:            Rails.application.secrets.username,
     password:             Rails.application.secrets.password,
     authentication:       'plain',
-    tls:                  true,
+    #tls:                  true,
     openssl_verify_mode:  OpenSSL::SSL::VERIFY_NONE,
     enable_starttls_auto: true  }
 
